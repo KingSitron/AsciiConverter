ab8cf3bf34e4e6e4ab010e185b72c3c400fe1d9f
KingSitron <simon.grohnert@gmail.com>
2023-06-28 21:39:11 +0200

    Finish and Comments

--- AsciiConverter.cpp
+++ AsciiConverter.cpp
@@ -8,6 +8,18 @@
 //   siehe: https://de.wikipedia.org/wiki/Sobel-Operator
 //   siehe: https://www.geeksforgeeks.org/edge-detection-using-prewitt-scharr-and-sobel-operator/amp/
 // - abspeichern
+//
+// 
+// - Labor 6 + 7
+// - AsciiConverter
+//   Der AsciiConverter nimmt als BMP gespeicherte Graustufen Bilddateien 
+//   und wandelt diese in ein Bild das aus Asciizeichen besteht.
+//   Durch eine Eingabe kann bestimmt werden wie groß das Bild ist, welches in der Ausgabe ausgegeben wird.
+//   (Je nach Font invertiert)
+// 
+// - Namen: Cedric Friedsch,Luis Geissler, Simon Grohnert
+// - Datum: 28.6.2023
+//
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
@@ -19,29 +31,9 @@ const char PATTERN02[] = "@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~
 const int PATTERNSIZE02 = sizeof(PATTERN02) / sizeof(PATTERN02[0]);
 const unsigned short VALID_FILETYPE = 0x4d42;
 const unsigned short VALID_BITCOUNT = 8;
-const int TARGET_RATIO = 2;
+const int TARGET_RATIO = 2;    //Asciicharakters sind in etwa doppelt so hoch wie breit
 
-
-void AusgabeArray(int* array[], int width, int height);
-
-struct HexCharStruct
-{
-	unsigned char c;
-	HexCharStruct(unsigned char _c) : c(_c) { }
-};
-
-inline std::ostream& operator<<(std::ostream& o, const HexCharStruct& hs)
-{
-	return (o << std::hex << (int)hs.c);
-}
-
-inline HexCharStruct hex(unsigned char _c)
-{
-	return HexCharStruct(_c);
-}
-
-struct BmpMetaData
-{
+struct BmpMetaData {
 	// durch Datei-Lesen gesetzt
 	unsigned char* Data;
 	unsigned int FileLen = 0;
@@ -55,8 +47,7 @@ struct BmpMetaData
 	unsigned int PaletteCount;		// 4 Bytes an Bytepos = 46
 };
 
-bool ReadFile(string fn, BmpMetaData& img)
-{
+bool ReadFile(string fn, BmpMetaData& img) {
 	// Datei im Stil "C" oeffnen
 	FILE* file = fopen(fn.c_str(), "rb");
 	if (file == NULL)
@@ -80,47 +71,35 @@ bool ReadFile(string fn, BmpMetaData& img)
 
 //Teständerung
 
-BmpMetaData DuplicateImage(BmpMetaData& img)
-{
-	BmpMetaData res = img;
-	res.Data = new unsigned char[res.FileLen];
-	memcpy(res.Data, img.Data, img.FileLen);
-	return res;
-}
-
-unsigned short Decode2Bytes(BmpMetaData& img, int pos)
-{
-	// Erstellen Sie hier ihren Code.
+unsigned short Decode2Bytes(BmpMetaData& img, int pos) {
+	//Addierung des zweiten Werts mithilfe des Shift Operators und einer Veroderung
 	return (img.Data[pos] | (img.Data[pos + 1]<<8));
 }
 
-unsigned int Decode4Bytes(BmpMetaData& img, int pos)
-{
-	// Erstellen Sie hier ihren Code.
+unsigned int Decode4Bytes(BmpMetaData& img, int pos) {
+	//Addieren des zweiten, dritten und vierten Wertes mithilfe des Shift Operators und Veroderung
 	return (img.Data[pos] |(img.Data[pos + 1]<<8) | (img.Data[pos + 2]<<16) | (img.Data[pos + 3]<<24));
 }
 
-bool DecodeHeader(BmpMetaData& img)
-{
-	// Erstellen Sie hier ihren Code.
+bool DecodeHeader(BmpMetaData& img) {
+	//Aufrufen der Position der Pixeldaten, der Höhe, der Breite und des PalettCounts
 	img.PosPixelData = Decode4Bytes(img, 10);
 	img.Width = Decode4Bytes(img, 18);
 	img.Height = Decode4Bytes(img, 22);
 	img.PaletteCount = Decode4Bytes(img, 46);
+	
 	img.FileType = Decode2Bytes(img, 0);
 	img.BitCount = Decode2Bytes(img, 28);
+	
+	//Überprüfen des FileTypes und des Bitcount
 	return (img.FileType == VALID_FILETYPE && img.BitCount == VALID_BITCOUNT);
 }
 
-unsigned char GetPixel(BmpMetaData& img, int x, int y){
+unsigned char GetPixel(BmpMetaData& img, int x, int y) {
+	//Aufrufen von Pixeldaten an Position x und y
 	return img.Data[img.PosPixelData+x+img.Width*y];
 }
 
-void PutPixel(BmpMetaData& img, int x, int y, unsigned char value)
-{
-	// Erstellen Sie hier ihren Code.
-	return;
-}
 int getAverage(int* eingabeMatrix[], int width, int height, int x, int y,int zoomWidth, int zoomHeight) {
 	float pixelPerCharWidth = float(width) / zoomWidth;
 	float pixelPerCharHeight = float(height) / zoomHeight;
@@ -128,25 +107,41 @@ int getAverage(int* eingabeMatrix[], int width, int height, int x, int y,int zoo
 	float zoomFactorHeight = float(zoomHeight) / float(height);
 	int sum = 0;
 	int count = 0;
+	//Ablaufen eines Feldes im Originalbildarray angepasst auf die eingegebene Größe
 	for (float x0=x* pixelPerCharWidth; x0 < pixelPerCharWidth* x + pixelPerCharWidth;x0 += zoomFactorWidth) {
 		for (float y0 = y * pixelPerCharHeight; y0 < pixelPerCharHeight* y + pixelPerCharHeight;y0 += zoomFactorHeight) {
 			sum += eingabeMatrix[int(x0)][int(y0)];
 				++count;
 		}
 	}
+	//Durchschnittserrechnung der Addierten Werte
 	return sum/count;
 }
-void AppyOperator3x3(int* eingabeMatrix[], int width, int height)
-{
-	//width = width - width % 3; //Überschuss der eingabe Matrix an dieser abziehen 
-	//hight = hight - hight % 6;
+
+char IntToAscii(int value) {
+	//Errechnen eines Characters aus einem Patterns in Abhängigkeit von eine Intwert
+	return PATTERN02[value % (PATTERNSIZE02 - 1)];
+	//Patternsize-1 da ein Wert mehr errechnet wurde und somit gab es sonst Lehrstallen im Bild
+}
+
+void AusgabeArray(int* array[], int width, int height) {
+	//Ausgabe lauft Pixeldaten in anderer Reihenfolge ab, damit das Bild bei der Ausgabe richtig angezeigt wird 
+	for (int iy = height - 1; iy >= 0; --iy) {
+		for (int ix = 0; ix < width; ++ix) {
+			cout << IntToAscii(array[ix][iy]);
+		}
+		cout << endl;
+	}
+}
+void CompressStartImgAndPrint(int* eingabeMatrix[], int width, int height) {
+	//Abfrage der breite des AsciiBildes
 	cout << "Geben sie die gewuenschte Breite ein (in Asciizeichen): " << endl;
 	int zoomWidth= 0;
 	cin >> zoomWidth;
 	
 
-
-	float zoomFactor = float(zoomWidth)/float(width);
+	//Errechnen der Asciibildgrößen aus Originalgrößen des Bildes
+	float zoomFactor = float(zoomWidth)/float(width); 
 	int zoomHeight = (height * zoomFactor)/ TARGET_RATIO;
 
 	int** kompressMatrix = new int* [zoomWidth]; //Matrix für speicherung neuer Pixel werte
@@ -154,73 +149,25 @@ void AppyOperator3x3(int* eingabeMatrix[], int width, int height)
 	for (int i = 0; i < zoomWidth; ++i) {
 		kompressMatrix[i] = new int[zoomHeight];
 	}
-	for (int x = 0; x < zoomWidth; x++) { //Geht immer die einzelnen pixel im 3x6 feld druch
+	//Ablaufen der Asciibildmatrix
+	for (int x = 0; x < zoomWidth; x++) { 
 		for (int y = 0; y <zoomHeight; y++) {
 
 			kompressMatrix[x][y] = getAverage(eingabeMatrix,width,height,x,y,zoomWidth,zoomHeight);
 
 		}
 	}
-	/*int oldPixelToNewWidth = 1 / zoomFactor;
-	int oldPixelToNewHeight = (1 / zoomFactor )* 2;
-	for (int i = 0; i < zoomWidth* zoomFactor; i+= oldPixelToNewWidth) { //Geht immer in dreierschitten durch in x Richtung 
-		for (int k = 0; k < zoomHeight* zoomFactor; k += oldPixelToNewHeight) { //Geht immer 6 pixel nach unten in y Richtung
-
-			int mittelwert = 0; //Speicherung der Mittelwerte
-
-			for (int x = i; x < i + oldPixelToNewWidth; x++) { //Geht immer die einzelnen pixel im 3x6 feld druch
-				for (int y = k; y < oldPixelToNewHeight; y++) {
-
-					mittelwert = mittelwert + eingabeMatrix[x][y];
-
-				}
-			}
-
-			kompressMatrix[(i / oldPixelToNewWidth)][(k / oldPixelToNewHeight)] = mittelwert / (oldPixelToNewWidth * oldPixelToNewHeight);
-
-		}
-	}*/
 	AusgabeArray(kompressMatrix, zoomWidth, zoomHeight);
 }
 
 
-//Ascii Ergänzung
 
-char PixelValueToChar(int mode, int value)
-{
-	// Ihr Code hier ...
-	return 0;
-}
 
-void GenerateAsciiArt(BmpMetaData& src, int numCols, int mode)
-// mode == 1 -> PATTERN01[] wird genutzt.
-//     sonst -> PATTERN02[] wird genutzt.
-{
-	// access
-	if (src.Height < 1 || src.Width < 1 || numCols < 1)
-		return;
 
-	// Ihr Code hier ...
-
-}
-
-char IntToAscii(int value) {
-	return PATTERN02[value % (PATTERNSIZE02-1)];
-}
-
-void AusgabeArray(int* array[],int width,int height) {
-	for (int iy = height-1; iy >=0 ; --iy) {
-		for (int ix = 0; ix < width; ++ix) {
-			cout<< IntToAscii(array[ix][iy]);
-		}
-		cout << endl;
-	}
-}
 
 
 int main()
 {
-	//Ascii Ergänzung
 	// Bildstruktur
 	BmpMetaData sourceImg = { 0 };
 
@@ -247,12 +194,6 @@ int main()
 			array[ix][iy] = GetPixel(sourceImg, ix ,iy);
 		}
 	}
+	CompressStartImgAndPrint(array, sourceImg.Width, sourceImg.Height);
 
-	//AusgabeArray(array, sourceImg.Width, sourceImg.Height);
-	AppyOperator3x3(array, sourceImg.Width, sourceImg.Height);
-
-
-
-	// convert
-	GenerateAsciiArt(sourceImg, 120, 1);
 }
